b987964c623eeb7cb39cf62806880dd15fe950ea
boparairaj <harkomalpalsingh1996@gmail.com>
2023-09-07 00:12:59 +0500

    Algorithm 2 has been implemented and half commented

--- Marks.java
+++ Marks.java
@@ -1,68 +1,125 @@
-
-/**
- * Write a description of class Marks here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
-
-import java.util.Scanner;
-public class Marks
-{
-    // instance variables - replace the example below with your own
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter the assignment name: ");
-        String assignmentName = scanner.nextLine();
-
-        double[] marksList = new double[30];
-        int studentCount = 0;
-
-        while (studentCount < 30) {
-            System.out.print("Enter mark for student " + (studentCount + 1) + ": ");
-            double mark = scanner.nextDouble();
-
-            if (mark >= 0 && mark <= 30) {
-                marksList[studentCount] = mark;
-                studentCount++;
-            } else {
-                System.out.println("Invalid input! Enter a mark between 0 and 30.");
+    
+    /**
+     * Write a description of class Marks here.
+     *
+     * @author Rajwant Kaur
+     * @version (04-09-2023)
+     */
+    
+    import java.util.Scanner;
+    public class Marks
+    {
+        // instance variables - replace the example below with your own
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+    
+            System.out.print("Enter the assignment name: ");
+            String assignmentName = scanner.nextLine();
+    
+            double[] marksList = new double[30];
+            int studentCount = 0;
+    
+            while (studentCount < 30) {
+                System.out.print("Enter mark for student " + (studentCount + 1) + ": ");
+                double mark = scanner.nextDouble();
+    
+                if (mark >= 0 && mark <= 30) {
+                    marksList[studentCount] = mark;
+                    studentCount++;
+                } else {
+                    System.out.println("Invalid input! Enter a mark between 0 and 30.");
+                }
             }
+    
+            System.out.println("Assignment Name: " + assignmentName);
+            for (double mark : marksList) {
+                System.out.print(mark + " ");
+            }
+            System.out.println();
+            
+            findHighestAndLowest(marksList);
+            calculateMeanAndStandardDeviation(marksList);
         }
-
-        System.out.println("Assignment Name: " + assignmentName);
-        for (double mark : marksList) {
-            System.out.print(mark + " ");
-        }
-        System.out.println();
         
-        findHighestAndLowest(marksList);
+        // ALgorithm 1: 
+            public static void findHighestAndLowest(double[] marks) {
+            // initialization of variables 
+            double highestMark = -1;
+            double lowestMark = 31;
+    
+            // Iterate through each marks
+            for (double mark : marks) {
+                
+                // Check current marks with highest marks if greater then assigned this marks to highest mark variable
+                if (mark > highestMark) {
+                    highestMark = mark;
+                }
+                // Check current marks with lowest marks if lower then assigned to lowest marks.
+                if (mark < lowestMark) {
+                    lowestMark = mark;
+                }
+            }
+    
+            // print highest marks 
+            System.out.println("Highest Mark: " + highestMark);
+            
+            //print lowest marks 
+            System.out.println("Lowest Mark: " + lowestMark);
+        }
+        
+         // compute absolute   
+        public static double manualAbs(double value) {
+        if(value < 0) {
+            return -value;
+        }
+        return value;
+    }
+        
+    // compute square root of number
+        public static double manualSqrt(double value) {
+        if(value == 0 || value == 1) {
+            return value;
+        }
+    // initialize variables
+        double precision = 0.00001;
+        double approximation = value;
+        double previous;
+    
+        do {
+            previous = approximation;
+            approximation = 0.5 * (approximation + value / approximation);
+        } while(manualAbs(approximation - previous) > precision);
+    
+        return approximation;
     }
     
-    // ALgorithm 1: 
-        public static void findHighestAndLowest(double[] marks) {
-        // initialization of variables 
-        double highestMark = -1;
-        double lowestMark = 31;
-
-        // Iterate through each marks
-        for (double mark : marks) {
+        public static void calculateMeanAndStandardDeviation(double[] marks) {
             
-            // Check current marks with highest marks if greater then assigned this marks to highest mark variable
-            if (mark > highestMark) {
-                highestMark = mark;
+            // initialize the variable
+            double total = 0;
+            double varianceSum = 0;
+    
+            // iterate through marks array to get the total marks
+            for (double mark : marks) {
+                total += mark;
             }
-            // Check current marks with lowest marks if lower then assigned to lowest marks.
-            if (mark < lowestMark) {
-                lowestMark = mark;
+    
+            // compute mean value
+            double mean = total / marks.length;
+    
+            
+            // iterate through each marks to find total variance
+            for (double mark : marks) {
+               varianceSum += (mark - mean) * (mark - mean);
             }
+    
+            // compute variance and standard deviation
+            double variance = varianceSum / marks.length;
+            double standardDeviation = manualSqrt(variance);
+    
+            //print mean, standard deviation marks
+            System.out.println("Variance:" + variance);
+            System.out.println("Mean: " + mean);
+            System.out.println("Standard Deviation: " + standardDeviation);
         }
-
-        // print highest marks 
-        System.out.println("Highest Mark: " + highestMark);
-        
-        //print lowest marks 
-        System.out.println("Lowest Mark: " + lowestMark);
     }
-}
